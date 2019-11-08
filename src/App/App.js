ca8aaffce1d0e2fced3e45ac1652d62f20c0c1c5
Phoumrin Tan <phoumrint@sbcglobal.net>
2019-11-08 01:18:57 -0800

    Renamed Workflow to WorkflowForm Added form section to WorkflowForm

--- src/App/App.js
+++ src/App/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 
 import Home from './Home';
-import Workflow from '../Workflow/Workflow';
+import WorkflowForm from '../Workflow/WorkflowForm';
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <div>
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route path="/workflows/:name" component={Workflow} />
+          <Route path="/workflows/:name" component={WorkflowForm} />
         </Switch>
       </div>
     </div>
