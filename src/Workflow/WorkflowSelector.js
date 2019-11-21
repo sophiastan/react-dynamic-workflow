5a05e037b091016609f440201cd0a1c86ad44741
Phoumrin Tan <phoumrint@sbcglobal.net>
2019-11-21 07:58:13 -0800

    Changed WorkflowSelector to get workflow name from url if it is available.

--- src/Workflow/WorkflowSelector.js
+++ src/Workflow/WorkflowSelector.js
@@ -1,23 +1,34 @@
 import React, { Component } from 'react';
 import AgreementForm from './AgreementForm';
 import SignService from '../Services/SignService';
+import WorkflowService from '../Services/WorkflowService';
 
 class WorkflowSelector extends Component {
 
     constructor(props) {
         super(props);
+
+        let workflowName = null;
+        if (props.match && props.match.params) {
+            workflowName = props.match.params.name;
+        }
+
         this.state = {
             signService: new SignService(),
+            workflowService: new WorkflowService(),
+            workflows: [],
+            workflowName: workflowName,
             workflow_id: null,
-            workflows: []
         };
     }
 
     async componentDidMount() {
         const workflows = await this.state.signService.getWorkflows();
+        const workflow_id = this.state.workflowService.getWorkflowId(workflows, this.state.workflowName);
 
         this.setState({
-            workflows: workflows
+            workflows: workflows,
+            workflow_id: workflow_id
         });
     }
 
